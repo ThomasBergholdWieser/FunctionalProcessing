97c5a1fc91d0a58921cc1ba992ed8b3eebb0dedf
Berghold-Wieser Thomas <thomas.berghold-wieser@tietoevry.com>
2023-03-23 15:59:56 +0100

    added logging pipeline, extension methods

---
+++ Source/FunctionalProcessing/Extensions/IExecutionResultExtensions.cs
@@ -0,0 +1,12 @@
+﻿using FunctionalProcessing.Interfaces;
+
+namespace FunctionalProcessing.Extensions;
+
+public static class IExecutionResultExtensions
+{
+    public static ExecutionResult Reduce(this IExecutionResult? source) =>
+        new() { Error = source?.Error };
+
+    public static Task<T> AsTask<T>(this T source) where T : IExecutionResult =>
+        Task.FromResult(source);
+}
---
+++ Source/FunctionalProcessing/Logging/FunctionalProcessingLoggingPipeline.cs
@@ -0,0 +1,29 @@
+﻿using FunctionalProcessing.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionalProcessing.Logging;
+
+public sealed class FunctionalProcessingLoggingPipeline<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<TRequest> logger;
+
+    public FunctionalProcessingLoggingPipeline(ILogger<TRequest> logger)
+    {
+        this.logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var response = await next();
+
+        if (this.logger.IsEnabled(LogLevel.Debug) &&
+            response is IExecutionResult { ExecutionFailed: true } executionResult)
+        {
+            this.logger.LogDebug(executionResult.CheckedError.Message);
+        }
+
+        return response;
+    }
+}
